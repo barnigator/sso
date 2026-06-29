3685c96383cbcb1db6ecd561a501b78873bc88ae
barnigator <lorivan@bk.ru>
2026-06-29 09:40:23 +0300

    error checks, migrations

--- go.mod
+++ go.mod
@@ -9,9 +9,11 @@ require (
 	github.com/barnigator/protos v0.0.1 // indirect
 	github.com/fatih/color v1.19.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	golang.org/x/crypto v0.53.0 // indirect
 	golang.org/x/net v0.55.0 // indirect
 	golang.org/x/sys v0.46.0 // indirect
--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -41,6 +41,8 @@ type AppProvider interface {
 
 var (
 	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrInvalidAppID       = errors.New("invalid app id")
+	ErrUserExists         = errors.New("user already exists")
 )
 
 // New returns a new instance of Auth service.
@@ -130,6 +132,11 @@ func (a *Auth) RegisterNewUser(ctx context.Context, email, password string) (int
 
 	id, err := a.usrSavor.SaveUser(ctx, email, passHash)
 	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			log.Warn("user already exists", sl.Err(err))
+
+			return 0, fmt.Errorf("%s: %w", fn, ErrUserExists)
+		}
 		log.Error("failed to save user", sl.Err(err))
 
 		return 0, fmt.Errorf("%s: %w", fn, err)
@@ -152,8 +159,12 @@ func (a *Auth) IsAdmin(ctx context.Context, userID int64) (bool, error) {
 
 	isAdmin, err := a.usrProvider.IsAdmin(ctx, userID)
 	if err != nil {
-		return false, fmt.Errorf("%s: %w", fn, err)
+		if errors.Is(err, storage.ErrAppNotFound) {
+			log.Warn("user not found", sl.Err(err))
 
+			return false, fmt.Errorf("%s: %w", fn, ErrInvalidAppID)
+		}
+		return false, fmt.Errorf("%s: %w", fn, err)
 	}
 
 	log.Info("checked if user is admin", slog.Bool("isAdmin", isAdmin))
